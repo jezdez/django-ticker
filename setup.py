fe8ae743df688ba54d518fa3167ee95c079cd87c
Jannis Leidel <jannis@leidel.info>
2009-03-12 18:18:19 +0100

    Fixed stupid setup.py name typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-ajax-validation',
+    name='django-ticker',
     version='0.1.0',
     description='A simple Django app for newsticker with a row level permission based workflow.',
     author='Martin Mahner',
